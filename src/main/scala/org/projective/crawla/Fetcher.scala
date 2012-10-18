5c4ab03d33e3175119b6708f4447f900338e1a42
rob <rob@Quasar.(none)>
2012-10-18 01:42:58 -0400

    adsf

--- src/main/scala/org/projective/crawla/Fetcher.scala
+++ src/main/scala/org/projective/crawla/Fetcher.scala
@@ -2,6 +2,7 @@ package org.projective.crawla
 
 import akka.dispatch.{Promise, Future}
 import akka.actor.{Actor, ActorRef, Props}
+import akka.pattern.pipe
 import cc.spray.can.client.HttpClient
 import cc.spray.client.HttpConduit
 import cc.spray.http._
@@ -9,39 +10,26 @@ import java.net.URL
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
-case class FetchRequest(url: URL)
-
 class Fetcher(httpClient: ActorRef) extends Actor {
   def receive = {
-    case FetchRequest(url) => Fetch(url, sender)
+    case url: URL => Fetch(url, sender)
   }
 
   def Fetch(url: URL, replyTo: ActorRef) {
     val host = url.getHost
     val path = url.getPath
 
-    context.system.log.info("Hit here 1")
-
     val conduit = context.actorOf(
       props = Props(new HttpConduit(httpClient, host)),
       name = "http-conduit")
 
-    val responseFuture = HttpConduit
-		    .sendReceive(conduit)(
-		       HttpRequest(method = HttpMethods.GET, uri = path)
-		    )
+    val request = HttpRequest(method = HttpMethods.GET, uri = path)
+    val responseFuture = HttpConduit.sendReceive(conduit)(request)
 
-    context.system.log.info("Response future created")
-    
-    import akka.pattern.pipe
-
-    (responseFuture map {
-	response  => 
-	  context.system.log.info("Mapping response")
-	  context.stop(conduit)
-	  val doc = Jsoup.parse(response.entity.asString)
-	  context.system.log.info("Got the doc, returning")
-          doc
+    (responseFuture 
+       map { response  => 
+	       context.stop(conduit)
+	       Jsoup.parse(response.entity.asString)
       }) pipeTo replyTo
   }
 }
